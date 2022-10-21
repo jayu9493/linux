3a4684a95c7408342f58e6108fbe044c592cde51
jayu9493 <87773845+jayu9493@users.noreply.github.com>
2022-10-21 19:18:40 +0530

    making install.sh
    So in this i started making the script file for installing and upgrading the system and programmes.

---
+++ install.md
@@ -0,0 +1,3 @@
+### <b>So this github repository tells about the great open source software and it's installation processes in one file. </b>
+
+Let's starts from the essential things like vlc,browser
---
+++ install.sh
@@ -0,0 +1,37 @@
+#sudo apt update || sudo apt upgrade
+
+#!/bin/bash
+echo "Type your package manager currently we support debian,rpm(fedora) and arch"
+echo "please type 1 for debian,2 for rpm & 3 for arch"
+read choice 
+function debian_upgrade {
+    sudo apt update || sudo apt upgrade
+}
+function rpm_upgrade {
+    echo "Do you want to check which packages are updating"
+    echo "Type yes or no"
+    read choice_rpm1
+    if [ $choice_rpm1 == yes ]; then
+        sudo dnf update #it will not work if you are on the other distribution 
+    fi
+}
+function arch_upgrade {
+    echo "Arch will update now"
+    sudo pacman -Syu
+}
+
+if [ $choice == 1 ]; then
+    echo "You have chosen debian section and we welcomes you"
+    echo "First we will upgrade this system"
+    debian_upgrade
+fi
+
+if [ $choice == 2 ]; then
+    echo "You have chosen fedora(rp) section and we welcomes you"
+    rpm_upgrade
+fi
+
+if [ $choice == 3 ]; then
+    echo "You have chosen Arch section and we welcomes you"
+    arch_upgrade
+fi
