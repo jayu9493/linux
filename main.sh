7926284f9287ac6f2a7102c9cd1fc0d16f10d7cc
jayu9493 <jayrpatel94@gmail.com>
2022-11-28 12:59:57 +0530

    now lets do these

--- main.sh
+++ main.sh
@@ -45,6 +45,22 @@ sudo apt install firefox -y
 
 function fedora{
     sudo dnf update -y && sudo dnf upgrade -y
+#firefox
+sudo dnf install firefox -y
+
+#chromiuim-browser
+sudo dnf install chromium --y
+
+#discord 
+sudo dnf install discord -y
+
+#vs-codium
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo -y
+sudo dnf install codium -y
+
+#okular
+sudo dnf install okular -y
 }
 #testing for github push
 #testing for withiout login github terminal
